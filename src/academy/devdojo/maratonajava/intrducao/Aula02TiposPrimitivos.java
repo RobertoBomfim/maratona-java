7d9d91c2cd8d4e94defb14ce5bb516b69c7d9a9f
Roberto Bomfim <bomfim062@gmail.com>
2022-08-10 17:10:31 -0300

    Aula03 exercício

--- src/academy/devdojo/maratonajava/intrducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/intrducao/Aula02TiposPrimitivos.java
@@ -2,15 +2,20 @@ package academy.devdojo.maratonajava.intrducao;
 
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
-        //variáveis Java: int, double, float, char, byte, short, long, boolean
+        //variáveis Java (tipos primitivos): int, double, float, char, byte, short, long, boolean
         int valor = 29;
         double altura = 1.77;
-        float peso = 61;
+        float peso = 61.50F;
         byte idadeByte = 10;
         short idadeShort = 10;
         boolean verdadeiro = true;
         boolean falso = false;
         char primeiraLetra = 'R';
+
+        //Não primitivo
+        String nome = "Roberto";
         System.out.println("O valor é " + valor + " reais");
+        System.out.println("Oi, meu nome é " + nome);
+
     }
 }
