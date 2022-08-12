4ddde895719ad00ab5527ef6f34156936e24014a
Roberto Bomfim <bomfim062@gmail.com>
2022-08-12 16:51:17 -0300

    Aulas introdução a classes

---
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/teste/EstudanteTeste01.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.teste;
+
+import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio.Estudante;
+
+public class EstudanteTeste01 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        estudante.nome = "Roberto";
+        estudante.idade = 29;
+        estudante.sexo = 'M';
+
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+
+        System.out.println(estudante);
+
+    }
+}
